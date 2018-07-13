a41ebdd74ae87d02a78c57cfb4a5bc22331137a1
Lê Nguyễn Chanh Tín <imchanhtin@gmail.com>
2018-07-13 21:18:52 +0700

    Cập nhật tình trạng và ảnh kèm theo

--- android/app/src/main/java/com/imchanhtin/canhbaochay/MainActivity.java
+++ android/app/src/main/java/com/imchanhtin/canhbaochay/MainActivity.java
@@ -3,11 +3,17 @@ package com.imchanhtin.canhbaochay;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.*;
+
+import pl.droidsonroids.gif.GifImageView;
+
 import static android.content.ContentValues.TAG;
 
 
@@ -15,10 +21,11 @@ public class MainActivity extends Activity {
     DatabaseReference nhietdo, doam, gas,co,khoi;
     TextView textNhiet, textDoam, textGas,textCO,textKhoi, textTinhtrang;
     double gtKhoi,gtCO,gtGAS, gtDOAM, gtNhiet;
-    int dem=0;
-
+    Image hinhanh;
+    MediaPlayer chay, baodong;
+    GifImageView anh;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textNhiet=findViewById(R.id.textNhiet);
@@ -27,7 +34,8 @@ public class MainActivity extends Activity {
         textCO=findViewById(R.id.textCO);
         textKhoi=findViewById(R.id.textKhoi);
         textTinhtrang = findViewById(R.id.textTR);
-
+        //anh = findViewById(R.id.anh);
+        //chay = MediaPlayer.create(getBaseContext(),R.raw.chay);
 
         //đọc dữ liệu theo node trong Firebase (cái này lấy từ nút canhbaochay/nhietdo)
         nhietdo = FirebaseDatabase.getInstance().getReference().child("nhietdo");
@@ -37,10 +45,21 @@ public class MainActivity extends Activity {
         khoi = FirebaseDatabase.getInstance().getReference().child("khoi");
         readDB();
         tinhtrang();
+       // listen();
 
     }
+//    public void listen(){
+//        anh.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(chay.isPlaying()== true){
+//                   chay.stop();
+//                }
+//                else chay.start();
+//            }
+//        });
+//    }
   public void readDB(){
-
         nhietdo.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -48,7 +67,6 @@ public class MainActivity extends Activity {
                 gtNhiet = value;
                 Log.v(TAG, "Value is: " + value);
                 textNhiet.setText(value+"");
-                dem++;
                 tinhtrang();
 
             }
@@ -65,8 +83,6 @@ public class MainActivity extends Activity {
               gtDOAM = dataSnapshot.getValue(Double.class);
               Log.v(TAG, "Value is: " + gtDOAM);
               textDoam.setText(gtDOAM+"");
-              dem++;
-
           }
 
           @Override
@@ -81,8 +97,6 @@ public class MainActivity extends Activity {
               gtGAS = dataSnapshot.getValue(Double.class);
               Log.v(TAG, "Value is: " + gtGAS);
               textGas.setText(gtGAS+"");
-              dem++;
-
           }
 
           @Override
@@ -97,8 +111,6 @@ public class MainActivity extends Activity {
               gtCO = dataSnapshot.getValue(Double.class);
               Log.v(TAG, "Value is: " + gtCO);
               textCO.setText(gtCO+"");
-              dem++;
-
           }
 
           @Override
@@ -114,8 +126,6 @@ public class MainActivity extends Activity {
               gtKhoi = value;
               Log.v(TAG, "Value is: " + value);
               textKhoi.setText(value+"");
-              dem++;
-
           }
 
           @Override
@@ -124,54 +134,50 @@ public class MainActivity extends Activity {
               Log.v(TAG, "Failed to read value.", error.toException());
           }
       });
-
   }
-
   public void tinhtrang(){
       if(gtNhiet <=27) {
           textTinhtrang.setText("Bình thường");
           textTinhtrang.setTextColor(Color.GREEN);
           mau(Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN);
+          anhtinhtrang(R.drawable.ok);
       }
       else if(gtNhiet > 36 && gtKhoi >= 10 && gtCO >= 10 || gtNhiet >50){
           textTinhtrang.setText("Cháy");
           textTinhtrang.setTextColor(Color.RED);
           mau(Color.RED,Color.RED,Color.RED,Color.RED,Color.RED);
+          anhtinhtrang(R.drawable.alert);
+
       }
       else if(gtNhiet >= 36 && gtNhiet <= 50) {
           textTinhtrang.setText("Nhiệt độ tăng cao");
           textTinhtrang.setTextColor(Color.YELLOW);
           mau(Color.YELLOW,Color.YELLOW,Color.GREEN,Color.GREEN,Color.GREEN);
+          anhtinhtrang(R.drawable.warring);
       }
       else if(gtDOAM > 95){
           textTinhtrang.setText("Độ ẩm cao");
           textTinhtrang.setTextColor(Color.YELLOW);
           mau(Color.GREEN,Color.RED,Color.GREEN,Color.GREEN,Color.GREEN);
+          anhtinhtrang(R.drawable.warring);
       }
       else if (gtGAS >= 15 && gtCO >= 20) {
           textTinhtrang.setText("Rò rỉ khí GAS!");
           textTinhtrang.setTextColor(Color.YELLOW);
           mau(Color.GREEN,Color.GREEN,Color.RED,Color.RED,Color.RED);
+          anhtinhtrang(R.drawable.warring);
       }
     }
-    public void mau(int NHIET, int DOAM, int GAS, int CO, int KHOI){
-//        TextView txtNhiet = findViewById(R.id.nhietdo);
-//        TextView txtdoam = findViewById(R.id.doam);
-//        TextView txtgas = findViewById(R.id.gas);
-//        TextView txtco = findViewById(R.id.co);
-//        TextView txtkhoi = findViewById(R.id.khoi);
-//
-//        txtNhiet.setTextColor(NHIET);
-//        txtdoam.setTextColor(DOAM);
-//        txtco.setTextColor(CO);
-//        txtkhoi.setTextColor(KHOI);
-//        txtgas.setTextColor(GAS);
-
+  public void mau(int NHIET, int DOAM, int GAS, int CO, int KHOI){
         textNhiet.setTextColor(NHIET);
         textDoam.setTextColor(DOAM);
         textGas.setTextColor(GAS);
         textCO.setTextColor(CO);
         textKhoi.setTextColor(KHOI);
+    }
+    public void anhtinhtrang(int dcAnh){
+        anh = findViewById(R.id.anh);
+        anh.setImageResource(dcAnh);
     }
 }
 
