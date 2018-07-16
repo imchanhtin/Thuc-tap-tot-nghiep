203bbefc8a4bb3a63e091587c0a4a5936b11b312
Lê Nguyễn Chanh Tín <imchanhtin@gmail.com>
2018-07-17 00:48:53 +0700

    thay đổi trạng thái nhạc thành công

--- android/app/src/main/java/com/imchanhtin/canhbaochay/MainActivity.java
+++ android/app/src/main/java/com/imchanhtin/canhbaochay/MainActivity.java
@@ -3,12 +3,11 @@ package com.imchanhtin.canhbaochay;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.SoundPool;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 
@@ -20,21 +19,27 @@ import static android.content.ContentValues.TAG;
 
 
 public class MainActivity extends Activity {
-    DatabaseReference nhietdo, doam, gas,co,khoi;
-    TextView textNhiet, textDoam, textGas,textCO,textKhoi, textTinhtrang;
-    double gtKhoi,gtCO,gtGAS, gtDOAM, gtNhiet;
+    DatabaseReference nhietdo, doam, gas, co, khoi;
+    TextView textNhiet, textDoam, textGas, textCO, textKhoi, textTinhtrang;
+    double gtKhoi, gtCO, gtGAS, gtDOAM, gtNhiet;
     GifImageView anh;
 
+    MediaPlayer chay, nguyhiem;
+
+
     @Override
-  protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textNhiet=findViewById(R.id.textNhiet);
-        textDoam=findViewById(R.id.textDoam);
-        textGas=findViewById(R.id.textGas);
-        textCO=findViewById (R.id.textCO);
-        textKhoi=findViewById(R.id.textKhoi);
+        textNhiet = findViewById(R.id.textNhiet);
+        textDoam = findViewById(R.id.textDoam);
+        textGas = findViewById(R.id.textGas);
+        textCO = findViewById(R.id.textCO);
+        textKhoi = findViewById(R.id.textKhoi);
         textTinhtrang = findViewById(R.id.textTR);
+
+        chay = MediaPlayer.create(this, R.raw.chay);
+        nguyhiem = MediaPlayer.create(this, R.raw.warning);
 
         //đọc dữ liệu theo node trong Firebase (cái này lấy từ nút canhbaochay/nhietdo)
         nhietdo = FirebaseDatabase.getInstance().getReference().child("nhietdo");
@@ -44,16 +49,17 @@ public class MainActivity extends Activity {
         khoi = FirebaseDatabase.getInstance().getReference().child("khoi");
         readDB();
         tinhtrang();
-
+       // nhac();
     }
-  public void readDB(){
+
+    public void readDB() {
         nhietdo.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 double value = dataSnapshot.getValue(Double.class);
                 gtNhiet = value;
                 Log.v(TAG, "Value is: " + value);
-                textNhiet.setText(value+"");
+                textNhiet.setText(value + "");
                 tinhtrang();
 
             }
@@ -65,138 +71,191 @@ public class MainActivity extends Activity {
             }
         });
         doam.addValueEventListener(new ValueEventListener() {
-          @Override
-          public void onDataChange(DataSnapshot dataSnapshot) {
-              gtDOAM = dataSnapshot.getValue(Double.class);
-              Log.v(TAG, "Value is: " + gtDOAM);
-              textDoam.setText(gtDOAM+"");
-          }
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                gtDOAM = dataSnapshot.getValue(Double.class);
+                Log.v(TAG, "Value is: " + gtDOAM);
+                textDoam.setText(gtDOAM + "");
+                tinhtrang();
+            }
 
-          @Override
-          public void onCancelled(DatabaseError error) {
-              // Failed to read value
-              Log.v(TAG, "Failed to read value.", error.toException());
-          }
-      });
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.v(TAG, "Failed to read value.", error.toException());
+            }
+        });
         gas.addValueEventListener(new ValueEventListener() {
-          @Override
-          public void onDataChange(DataSnapshot dataSnapshot) {
-              gtGAS = dataSnapshot.getValue(Double.class);
-              Log.v(TAG, "Value is: " + gtGAS);
-              textGas.setText(gtGAS+"");
-          }
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                gtGAS = dataSnapshot.getValue(Double.class);
+                Log.v(TAG, "Value is: " + gtGAS);
+                textGas.setText(gtGAS + "");
+                tinhtrang();
+            }
 
-          @Override
-          public void onCancelled(DatabaseError error) {
-              // Failed to read value
-              Log.v(TAG, "Failed to read value.", error.toException());
-          }
-      });
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.v(TAG, "Failed to read value.", error.toException());
+            }
+        });
         co.addValueEventListener(new ValueEventListener() {
-          @Override
-          public void onDataChange(DataSnapshot dataSnapshot) {
-              gtCO = dataSnapshot.getValue(Double.class);
-              Log.v(TAG, "Value is: " + gtCO);
-              textCO.setText(gtCO+"");
-          }
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                gtCO = dataSnapshot.getValue(Double.class);
+                Log.v(TAG, "Value is: " + gtCO);
+                textCO.setText(gtCO + "");
+                tinhtrang();
+            }
 
-          @Override
-          public void onCancelled(DatabaseError error) {
-              // Failed to read value
-              Log.v(TAG, "Failed to read value.", error.toException());
-          }
-      });
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.v(TAG, "Failed to read value.", error.toException());
+            }
+        });
         khoi.addValueEventListener(new ValueEventListener() {
-          @Override
-          public void onDataChange(DataSnapshot dataSnapshot) {
-              double value = dataSnapshot.getValue(Double.class);
-              gtKhoi = value;
-              Log.v(TAG, "Value is: " + value);
-              textKhoi.setText(value+"");
-          }
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                double value = dataSnapshot.getValue(Double.class);
+                gtKhoi = value;
+                Log.v(TAG, "Value is: " + value);
+                textKhoi.setText(value + "");
+                tinhtrang();
+            }
 
-          @Override
-          public void onCancelled(DatabaseError error) {
-              // Failed to read value
-              Log.v(TAG, "Failed to read value.", error.toException());
-          }
-      });
-  }
-  public void tinhtrang(){
-      if(gtNhiet <=27) {
-          textTinhtrang.setText("Bình thường");
-          textTinhtrang.setTextColor(Color.GREEN);
-          mau(Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN);
-          anhtinhtrang(R.drawable.ok);
-      }
-      else if(gtNhiet > 36 && gtKhoi >= 10 && gtCO >= 10 || gtNhiet >50){
-          textTinhtrang.setText("Cháy");
-          textTinhtrang.setTextColor(Color.RED);
-          mau(Color.RED,Color.RED,Color.RED,Color.RED,Color.RED);
-          anhtinhtrang(R.drawable.alert);
-          nhacChay();
-      }
-      else if(gtNhiet >= 36 && gtNhiet <= 50) {
-          textTinhtrang.setText("Nhiệt độ tăng cao");
-          textTinhtrang.setTextColor(Color.YELLOW);
-          mau(Color.YELLOW,Color.YELLOW,Color.GREEN,Color.GREEN,Color.GREEN);
-          anhtinhtrang(R.drawable.warring);
-          nhacNH();
-
-      }
-      else if(gtDOAM > 95){
-          textTinhtrang.setText("Độ ẩm cao");
-          textTinhtrang.setTextColor(Color.YELLOW);
-          mau(Color.GREEN,Color.RED,Color.GREEN,Color.GREEN,Color.GREEN);
-          anhtinhtrang(R.drawable.warring);
-          nhacNH();
-      }
-      else if (gtGAS >= 15 && gtCO >= 20) {
-          textTinhtrang.setText("Rò rỉ khí GAS!");
-          textTinhtrang.setTextColor(Color.YELLOW);
-          mau(Color.GREEN,Color.GREEN,Color.RED,Color.RED,Color.RED);
-          anhtinhtrang(R.drawable.warring);
-          nhacNH();
-      }
-      else if(gtKhoi > 40){
-          textTinhtrang.setText("Nguy hiểm");
-          textTinhtrang.setTextColor(Color.YELLOW);
-          mau(Color.GREEN,Color.YELLOW,Color.GREEN,Color.YELLOW,Color.RED);
-          anhtinhtrang(R.drawable.warring);
-          nhacNH();
-      }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.v(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
-  public void mau(int NHIET, int DOAM, int GAS, int CO, int KHOI){
+    public void tinhtrang() {
+
+        if (gtNhiet <= 27 && gtCO <20) {
+            textTinhtrang.setText("Bình thường");
+            textTinhtrang.setTextColor(Color.GREEN);
+            mau(Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN);
+            anhtinhtrang(R.drawable.ok);
+            if(nguyhiem.isLooping()){
+                nguyhiem.stop();
+                nguyhiem = MediaPlayer.create(this,R.raw.warning);
+            }
+            if(chay.isLooping()){
+                chay.stop();
+                chay = MediaPlayer.create(this,R.raw.chay);
+            }
+        } else if (gtNhiet > 36 && gtKhoi >= 10 && gtCO >= 10 || gtNhiet > 50) {
+            textTinhtrang.setText("Cháy");
+            textTinhtrang.setTextColor(Color.RED);
+            mau(Color.RED, Color.RED, Color.RED, Color.RED, Color.RED);
+            anhtinhtrang(R.drawable.alert);
+            chay.start();
+            chay.setLooping(true);
+            if(nguyhiem.isLooping()){
+                nguyhiem.setLooping(false);
+                nguyhiem.stop();
+                nguyhiem = MediaPlayer.create(this,R.raw.warning);
+            }
+        } else if (gtNhiet >= 36 && gtNhiet <= 50) {
+            textTinhtrang.setText("Nhiệt độ tăng cao");
+            textTinhtrang.setTextColor(Color.YELLOW);
+            mau(Color.YELLOW, Color.YELLOW, Color.GREEN, Color.GREEN, Color.GREEN);
+            anhtinhtrang(R.drawable.nguyhiem);
+            nguyhiem.start();
+            nguyhiem.setLooping(true);
+            if(chay.isLooping()){
+                chay.setLooping(false);
+                chay.stop();
+                chay = MediaPlayer.create(this,R.raw.chay);
+            }
+        } else if (gtDOAM > 95 ) {
+            textTinhtrang.setText("Độ ẩm cao");
+            textTinhtrang.setTextColor(Color.YELLOW);
+            mau(Color.GREEN, Color.RED, Color.GREEN, Color.GREEN, Color.GREEN);
+            anhtinhtrang(R.drawable.nguyhiem);
+            nguyhiem.start();
+            nguyhiem.setLooping(true);
+            if(chay.isLooping()){
+                chay.setLooping(false);
+                chay.stop();
+                chay = MediaPlayer.create(this,R.raw.chay);
+            }
+        } else if (gtGAS >= 15 && gtCO >= 20) {
+            textTinhtrang.setText("Rò rỉ khí GAS!");
+            textTinhtrang.setTextColor(Color.YELLOW);
+            mau(Color.GREEN, Color.GREEN, Color.RED, Color.RED, Color.RED);
+            anhtinhtrang(R.drawable.nguyhiem);
+            nguyhiem.start();
+            nguyhiem.setLooping(true);
+            if(chay.isLooping()){
+                chay.setLooping(false);
+                chay.stop();
+                chay = MediaPlayer.create(this,R.raw.chay);
+            }
+        } else if (gtKhoi > 40) {
+            textTinhtrang.setText("Nguy hiểm");
+            textTinhtrang.setTextColor(Color.YELLOW);
+            mau(Color.GREEN, Color.YELLOW, Color.GREEN, Color.YELLOW, Color.RED);
+            anhtinhtrang(R.drawable.nguyhiem);
+            nguyhiem.start();
+            nguyhiem.setLooping(true);
+            if(chay.isLooping()){
+                chay.setLooping(false);
+                chay.stop();
+                chay = MediaPlayer.create(this,R.raw.chay);
+            }
+        }
+    }
+    public void mau(int NHIET, int DOAM, int GAS, int CO, int KHOI) {
         textNhiet.setTextColor(NHIET);
         textDoam.setTextColor(DOAM);
         textGas.setTextColor(GAS);
         textCO.setTextColor(CO);
         textKhoi.setTextColor(KHOI);
     }
-  public void anhtinhtrang(int dcAnh){
+    public void anhtinhtrang(int dcAnh) {
         anh = findViewById(R.id.anh);
         anh.setImageResource(dcAnh);
     }
-  public void nhacChay(){
-      MediaPlayer chay,nguyhiem;
-      chay= MediaPlayer.create(this, R.raw.chay);
-      nguyhiem= MediaPlayer.create(this, R.raw.warning);
-      if(nguyhiem.isLooping()){
-          nguyhiem.setLooping(false);
-          nguyhiem.stop();
-      }
-      chay.start();
-      chay.setLooping(true);
-  }
-  public void nhacNH(){
-      MediaPlayer chay,nguyhiem;
-      chay= MediaPlayer.create(this, R.raw.chay);
-      nguyhiem= MediaPlayer.create(this, R.raw.warning);
-      if(chay.isLooping()){
-          chay.stop();
-      }
-      nguyhiem.start();
-      nguyhiem.setLooping(true);
-  }
+    public void nhac(){
+        textTinhtrang.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    chay.stop();
+                    nguyhiem.stop();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(textTinhtrang.toString().equals("Cháy")){
+                    chay.start();
+                    chay.setLooping(true);
+                    if(nguyhiem.isLooping()){
+                        nguyhiem.setLooping(false);
+                        nguyhiem.stop();
+                    }
+                }
+                if(textTinhtrang.toString().equals("Bình thường")){
+                    chay.stop();
+                    nguyhiem.stop();
+                }
+                if(!textTinhtrang.toString().equals("Cháy") && !textTinhtrang.toString().equals("Bình thường") ){
+                    nguyhiem.start();
+                    nguyhiem.setLooping(true);
+                    if(chay.isLooping()){
+                        chay.setLooping(false);
+                        chay.stop();
+                    }
+                }
+            }
+        });
+    }
 }
 
