76eec77f0f17c69b3dd6bf911fb0098b4723b48d
Lê Nguyễn Chanh Tín <imchanhtin@gmail.com>
2018-07-24 01:32:59 +0700

    cập nhật đường dẫn mặc định của web

--- apps/controllers/blog.js
+++ apps/controllers/blog.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 router.get("/",function(req,res){
     res.json({"message":"Day la trang blog"});
+    res.render("blog/index");
 });
 module.exports=router;
--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -5,7 +5,7 @@ router.use("/admin",require(__dirname+"/admin.js"));
 router.use("/blog",require(__dirname+"/blog.js"));
 
 router.get("/", function(req,res){
-    res.render("test");
+    res.render("index");
 });
 
 module.exports = router;
